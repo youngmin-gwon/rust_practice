076b72739c6d07614529c49449d426685e207969
Youngmin Gwon <youngmin.gwon@gmail.com>
2024-03-18 00:05:25 +0900

    feat: use generic

--- src/ch10/generics.rs
+++ src/ch10/generics.rs
@@ -1,39 +1,42 @@
 pub fn run() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest::<i32>(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest::<char>(&char_list);
     println!("The largest char is {}", result);
 }
 
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
+// -- code below is quite annoying since it looks quite duplicated.
+// fn largest_i32(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
 
-    largest
-}
+//     largest
+// }
 
-fn largest_char(list: &[char]) -> &char {
-    let mut largest = &list[0];
+// fn largest_char(list: &[char]) -> &char {
+//     let mut largest = &list[0];
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
 
-    largest
-}
+//     largest
+// }
+// --
 
+// use generic instead
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
