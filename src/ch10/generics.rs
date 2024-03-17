4eff21438007aead131d90718952bde9091b1359
Youngmin Gwon <youngmin.gwon@gmail.com>
2024-03-17 23:59:51 +0900

    feat: add generic

--- src/ch10/generics.rs
+++ src/ch10/generics.rs
@@ -33,3 +33,15 @@ fn largest_char(list: &[char]) -> &char {
 
     largest
 }
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
